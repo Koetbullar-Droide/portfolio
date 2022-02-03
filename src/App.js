49c8fd807850a6411c39738470d56b8df2168878
jannik <jannik.6@gmx.ch>
2022-02-03 17:43:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TopNavigation from "./components/TopNavigation";
+function App() {
+  return (
+    <div className="flex">
+      <TopNavigation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TopNavigation/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function TopNavigation() {
+  return <h1>TopNavigation</h1>
+}
+
+export default TopNavigation;
